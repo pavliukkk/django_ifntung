df5fb01774d770ebe5bb4c79797876a882332c23
m1racle <petro.pavliuk-pi2002@kep.nung.edu.ua>
2025-11-22 17:56:26 +0200

    Updated Django settings and URLs

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default':{
+    'ENGINE': 'django.db.backends.mysql', 'NAME': 'mysite_db',
+    'USER': 'mysite_usr', 'PASSWORD':'mysite_pass',
     }
 }
+
 
 
 # Password validation
